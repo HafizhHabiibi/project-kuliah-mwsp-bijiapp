ab4ec695e2818913adb1857e9d377e0830112546
Hafizh Habiibi Lubis <hafizhhabiibi123@gmail.com>
2025-12-25 11:31:59 +0700

    feat(api): menambahkan endpoint produk CRUD

--- laravel_sanctum/routes/api.php
+++ laravel_sanctum/routes/api.php
@@ -1,13 +1,23 @@
 <?php
 
-Route::group(['namespace' => 'App\Http\Controllers\API'], function () {
-    // --------------- Register and Login ----------------//
-    Route::post('register', 'AuthenticationController@register')->name('register');
-    Route::post('login', 'AuthenticationController@login')->name('login');
-    
-    // ------------------ Get Data ----------------------//
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::get('get-user', 'AuthenticationController@userInfo')->name('get-user');
-        Route::post('logout', 'AuthenticationController@logOut')->name('logout');
-    });
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthenticationController;
+use App\Http\Controllers\API\ProductController;
+
+// --------------- Register & Login (PUBLIC) ----------------
+Route::post('/register', [AuthenticationController::class, 'register']);
+Route::post('/login', [AuthenticationController::class, 'login']);
+
+// --------------- Protected Routes (SANCTUM) ----------------
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Auth
+    Route::get('/get-user', [AuthenticationController::class, 'userInfo']);
+    Route::post('/logout', [AuthenticationController::class, 'logOut']);
+
+    // Products CRUD
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 });
